7fbfb5a429d8b26b244ae685ae131c8cbcc9d436
Sergey Popov <sergei.popov174@gmail.com>
2020-12-04 00:18:57 +0500

    support class, first test

--- cc.py
+++ cc.py
@@ -43,20 +43,27 @@ class GeneralVisitor(ast.NodeVisitor):
         func_vis = GeneralVisitor()
         for child in node.body:
             func_vis.visit(child)
-            self.function[node.name] = func_vis.metrics
+            func_name = f"{node.parent.name}.{node.name}" if hasattr(node, "parent") else node.name
+            self.function[func_name] = func_vis.metrics
         self.generic_visit(node)
 
     def visit_AsyncFunctionDef(self, node):
         self.visit_FunctionDef(node)
 
 
+def calc_cyclomatic(source):
+    root = ast.parse(source)
+    for node in ast.walk(root):
+        for child in ast.iter_child_nodes(node):
+            if isinstance(node, ast.ClassDef):
+                child.parent = node
+    visitor = GeneralVisitor()
+    visitor.visit(root)
+    return visitor
 
 
-with open("test.py", "r") as file:
-    source = file.read()
 
-root = ast.parse(source)
-visitor = GeneralVisitor()
-visitor.visit(root)
+
+visitor = calc_cyclomatic(source)
 print(visitor.metrics)
 print(visitor.function)
