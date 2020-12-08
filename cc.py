40d6f0688a675c5a367850d9290c73bea322150f
Sergey Popov <sergei.popov174@gmail.com>
2020-12-08 13:55:40 +0500

    add parse, dir calc metrics

--- cc.py
+++ cc.py
@@ -1,9 +1,16 @@
 import ast
+import os
+import argparse
+from typing import Dict
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", required=True, type=str)
+args = parser.parse_args()
 
 class GeneralVisitor(ast.NodeVisitor):
 
-    def __init__(self):
-        self.function = {}
+    def __init__(self, filename):
+        self.filename = filename
+        self.function: Dict[str, int] = {}
         self.metrics = 0
 
     def visit_IfExp(self, node):
@@ -39,7 +46,7 @@ class GeneralVisitor(ast.NodeVisitor):
         self.generic_visit(node)
 
     def visit_FunctionDef(self, node):
-        func_vis = GeneralVisitor()
+        func_vis = GeneralVisitor(None)
         for child in node.body:
             func_vis.visit(child)
             func_name = f"{node.parent.name}.{node.name}" if hasattr(node, "parent") else node.name
@@ -49,13 +56,54 @@ class GeneralVisitor(ast.NodeVisitor):
     def visit_AsyncFunctionDef(self, node):
         self.visit_FunctionDef(node)
 
+    def __repr__(self):
+        res = ""
+        res += f"{self.filename} - {self.metrics}"
+        sort_func = dict(sorted(self.function.items(), key=lambda x: x[1]))
+        for func in sort_func:
+            res += f"\n\t{func} - {sort_func[func]}"
+        return res
 
-def calc_cyclomatic(source):
+def calc_cyclomatic(source: str, filename: str):
     root = ast.parse(source)
     for node in ast.walk(root):
         for child in ast.iter_child_nodes(node):
             if isinstance(node, ast.ClassDef):
                 child.parent = node
-    visitor = GeneralVisitor()
+    visitor = GeneralVisitor(filename)
     visitor.visit(root)
     return visitor
+
+
+def get_python_files(path):
+    py_files_path = []
+    for root, dirs, files in os.walk(path):
+        if not files:
+            continue
+        for file in files:
+            if file[-2:] == "py":
+                py_files_path.append(os.path.join(root, file))
+    return py_files_path
+
+
+def calc_dir(path):
+    py_files_path = get_python_files(path)
+    total_complexity = 0
+    for py_file in py_files_path:
+        with open(py_file, "r") as file:
+            source = file.read()
+            visitor = calc_cyclomatic(source, py_file)
+            print(visitor)
+            total_complexity += visitor.metrics
+    print('----------------------------')
+    print(total_complexity)
+
+
+def main():
+    src = args.s
+    calc_dir(src)
+
+
+if __name__ == "__main__":
+    main()
+
